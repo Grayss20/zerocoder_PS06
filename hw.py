53051a701e769044a323e7a2542e37d2003e5236
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-08-24 14:48:55 +0100

    final

--- hw.py
+++ hw.py
@@ -17,9 +17,9 @@ parsed_data = []
 
 for svet in svets:
     try:
-        name = svet.find_element(By.CSS_SELECTOR, "a span::text")
-        price = svet.find_element(By.CSS_SELECTOR, "span.ui-LD-ZU.KIkOH::text")
-        link = svet.find_element(By.CSS_SELECTOR, "a::attr(href)")
+        name = svet.find_element(By.CSS_SELECTOR, "a.ui-GPFV8").text
+        price = svet.find_element(By.CSS_SELECTOR, "span.ui-LD-ZU.KIkOH").text
+        link = svet.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
     except:
         print("Parsing error")
         continue
@@ -27,6 +27,9 @@ for svet in svets:
     parsed_data.append([name, price, link])
 
 driver.quit()
+
+for i in range(len(parsed_data)):
+    parsed_data[i][1] = float(parsed_data[i][1].replace("руб.", "").strip().replace(" ", ""))
 
 with open("svet.csv", "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
